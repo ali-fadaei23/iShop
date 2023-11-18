9deb4ee59ef3f1185e6843d5b27e252c0ee7a016
alifadaei <razorn1375@gmail.com>
2023-11-18 12:43:42 +0330

    Add hint for login

--- src/components/auth-components/sign-in/Login.jsx
+++ src/components/auth-components/sign-in/Login.jsx
@@ -14,14 +14,27 @@ import {
   FormControl,
   InputLabel,
   OutlinedInput,
+  InputAdornment,
+  IconButton,
 } from "@mui/material";
 import LoginShopImage from "../../../assets/img/login.png";
+import CopyToClipboard from "../../copy-to-clipboard/CopyToClipboard";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 const Login = () => {
   let navigate = useNavigate();
   let auth = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
+
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const handleUserName = (e) => {
     setUserName(e.target.value);
@@ -43,166 +56,183 @@ const Login = () => {
   }, [auth.user, auth.loading, navigate]);
 
   return (
-    <Grid
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-      container
-    >
-      <div className="container-shop">
-        <div className="container-shop-img">
-          <img
-            className="img-shop-login"
-            src={LoginShopImage}
-            alt="Login Shop"
-          />
-        </div>
-      </div>
+    <>
       <Grid
         sx={{
-          width: "40%",
           display: "flex",
-          alignItems: "center",
           justifyContent: "center",
+          alignItems: "center",
         }}
+        container
       >
-        <Box
+        <div className="container-shop">
+          <div className="container-shop-img">
+            <img
+              className="img-shop-login"
+              src={LoginShopImage}
+              alt="Login Shop"
+            />
+          </div>
+        </div>
+        <Grid
           sx={{
-            my: 8,
-            mx: 4,
+            width: "40%",
             display: "flex",
-            flexDirection: "column",
             alignItems: "center",
-            width: "60%",
+            justifyContent: "center",
           }}
         >
-          <Typography
-            sx={{ fontSize: "60px", fontWeight: "bold" }}
-            component="h1"
-            variant="h5"
-          >
-            Sign in
-          </Typography>
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
             sx={{
-              mt: 1,
+              my: 8,
+              mx: 4,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
+              width: "60%",
             }}
           >
-            <FormControl sx={{ width: "280px", marginTop: "20px" }}>
-              <InputLabel
-                sx={{
-                  fontSize: "small",
-                  fontWeight: "400",
-                }}
-                htmlFor="username"
-              >
-                Username
-              </InputLabel>
-              <OutlinedInput
-                sx={{ borderRadius: "30px" }}
-                value={userName}
-                onChange={handleUserName}
-                type="text"
-                size="small"
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="User Name"
-                color="secondary"
-                autoComplete="User Name"
-                autoFocus
-              />
-            </FormControl>
-            <FormControl sx={{ width: "280px", marginTop: "20px" }}>
-              <InputLabel
-                sx={{
-                  fontSize: "small",
-                  fontWeight: "400",
-                }}
-                htmlFor="outlined-adornment-lastname"
-              >
-                Password
-              </InputLabel>
-              <OutlinedInput
-                sx={{ borderRadius: "30px" }}
-                value={password}
-                onChange={handlePassword}
-                id="password"
-                type="password"
-                size="small"
-                color="secondary"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                autoComplete="current-password"
-              />
-            </FormControl>
-
-            <FormControlLabel
-              sx={{ fontSize: "small", fontWeight: "400" }}
-              control={
-                <Checkbox value="remember" size="small" color="primary" />
-              }
-              label="Remember me"
-            />
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                type="submit"
-                variant="contained"
-                sx={{
-                  mt: 3,
-                  mb: 2,
-                  backgroundColor: "#202020",
-                  height: "3rem",
-                  width: " 12rem",
-                  fontSize: "inherit",
-                  borderRadius: "1.625rem",
-                  fontWeight: "700",
-                }}
-                className="button-login"
-                disabled={auth.loading}
-              >
-                Sign In
-                {auth.loading && (
-                  <CircularProgress
-                    size={24}
-                    sx={{
-                      position: "absolute",
-                    }}
-                  />
-                )}
-              </Button>
-            </div>
-
-            <Grid
-              sx={{
-                marginTop: "30px",
-                display: "flex",
-                justifyContent: "center;",
-              }}
-              container
+            <Typography
+              sx={{ fontSize: "60px", fontWeight: "bold" }}
+              component="h1"
+              variant="h5"
             >
-              <Grid item>
-                <Link style={{ color: "#202020" }} to={"/signup"}>
-                  {"Don't have an account? Sign Up"}
-                </Link>
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{
+                mt: 1,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <FormControl sx={{ width: "280px", marginTop: "20px" }}>
+                <InputLabel
+                  sx={{
+                    fontSize: "small",
+                    fontWeight: "400",
+                  }}
+                  htmlFor="username"
+                >
+                  Username
+                </InputLabel>
+                <OutlinedInput
+                  sx={{ borderRadius: "30px" }}
+                  value={userName}
+                  onChange={handleUserName}
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="User Name"
+                  color="secondary"
+                  autoComplete="User Name"
+                  autoFocus
+                />
+              </FormControl>
+              <FormControl sx={{ width: "280px", marginTop: "20px" }}>
+                <InputLabel
+                  sx={{
+                    fontSize: "small",
+                    fontWeight: "400",
+                  }}
+                  htmlFor="outlined-adornment-lastname"
+                >
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  sx={{ borderRadius: "30px" }}
+                  value={password}
+                  onChange={handlePassword}
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  size="small"
+                  color="secondary"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  autoComplete="current-password"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+
+              <FormControlLabel
+                sx={{ fontSize: "small", fontWeight: "400" }}
+                control={
+                  <Checkbox value="remember" size="small" color="primary" />
+                }
+                label="Remember me"
+              />
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  sx={{
+                    mt: 3,
+                    mb: 2,
+                    backgroundColor: "#202020",
+                    height: "3rem",
+                    width: " 12rem",
+                    fontSize: "inherit",
+                    borderRadius: "1.625rem",
+                    fontWeight: "700",
+                  }}
+                  className="button-login"
+                  disabled={auth.loading}
+                >
+                  Sign In
+                  {auth.loading && (
+                    <CircularProgress
+                      size={24}
+                      sx={{
+                        position: "absolute",
+                      }}
+                    />
+                  )}
+                </Button>
+              </div>
+
+              <Grid
+                sx={{
+                  marginTop: "30px",
+                  display: "flex",
+                  justifyContent: "center;",
+                }}
+                container
+              >
+                <Grid item>
+                  <Link style={{ color: "#202020" }} to={"/signup"}>
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
+        </Grid>
       </Grid>
-    </Grid>
+      <div>
+        <CopyToClipboard />
+      </div>
+    </>
   );
 };
 
--- src/components/copy-to-clipboard/CopyToClipboard.jsx
+++ src/components/copy-to-clipboard/CopyToClipboard.jsx
@@ -1,7 +1,163 @@
+import {
+  Card,
+  CardContent,
+  TextField,
+  IconButton,
+  OutlinedInput,
+  FormControl,
+  InputLabel,
+  InputAdornment,
+  Typography,
+} from "@mui/material";
+import { ReactComponent as CopyIcon } from "../../assets/img/copy-icon.svg";
+
 import "./CopyToClipboard.css";
+import { useState } from "react";
 
 const CopyToClipboard = () => {
-  return <></>;
+  const textHint = `Use the following username and password to log in:`;
+  const hintLogin = { username: "johnd", password: "m38rmF$" };
+
+  const [copyUser, setCopyUser] = useState(false);
+  const [copyPass, setCopyPass] = useState(false);
+
+  const copyUserName = () => {
+    navigator.clipboard.writeText(hintLogin.username);
+    setCopyUser(true);
+  };
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(hintLogin.password);
+    setCopyPass(true);
+  };
+  return (
+    <div className="container-clipboard">
+      <Card
+        sx={{
+          width: "30%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "2rem",
+          padding: "0.9rem",
+        }}
+      >
+        <div style={{ width: "100%" }}>
+          <Typography
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              fontWeight: "bold",
+              fontSize: "2rem",
+            }}
+          >
+            Hint!
+          </Typography>
+        </div>
+        <div style={{ width: "100%" }}>
+          <Typography
+            sx={{
+              fontSize: "0.9rem",
+              display: "flex",
+              justifyContent: "flex-start",
+            }}
+          >
+            {textHint}
+          </Typography>
+        </div>
+        <CardContent>
+          <div>
+            <FormControl
+              sx={{ width: "280px", marginTop: "20px" }}
+              variant="outlined"
+            >
+              <InputLabel
+                sx={{
+                  fontSize: "small",
+                  fontWeight: "400",
+                }}
+                htmlFor="outlined-adornment-username"
+              >
+                User Name
+              </InputLabel>
+              <OutlinedInput
+                readOnly
+                sx={{ borderRadius: "30px" }}
+                size="small"
+                id="outlined-adornment-username"
+                type="text"
+                // defaultValue=""
+                defaultValue={hintLogin.username}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      sx={{ marginRight: 0 }}
+                      aria-label="toggle username visibility"
+                      onClick={copyUserName}
+                      edge="end"
+                    >
+                      {copyUser ? (
+                        <span style={{ fontSize: "14px", fontWeight: "bold" }}>
+                          Copied
+                        </span>
+                      ) : (
+                        <CopyIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="User Name"
+              />
+            </FormControl>
+          </div>
+          <div>
+            <FormControl
+              sx={{ width: "280px", marginTop: "20px" }}
+              variant="outlined"
+            >
+              <InputLabel
+                sx={{
+                  fontSize: "small",
+                  fontWeight: "400",
+                }}
+                htmlFor="outlined-adornment-password"
+              >
+                Password
+              </InputLabel>
+              <OutlinedInput
+                readOnly
+                sx={{ borderRadius: "30px" }}
+                size="small"
+                id="outlined-adornment-password"
+                type="text"
+                defaultValue={hintLogin.password}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      sx={{ marginRight: 0 }}
+                      aria-label="toggle password visibility"
+                      onClick={copyPassword}
+                      edge="end"
+                    >
+                      {copyPass ? (
+                        <span style={{ fontSize: "14px", fontWeight: "bold" }}>
+                          Copied
+                        </span>
+                      ) : (
+                        <CopyIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default CopyToClipboard;
